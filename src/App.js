e6e5e1e8fc7791bb5ba914b7810176f4fc447ecb
Baibhavop <baibhavop@gmail.com>
2024-03-30 02:59:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { marked } from 'marked';
+import './App.css';
+
+function Popup({ details, onClose }) {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+      <div className="bg-white p-4 rounded-lg">
+        <h2>{details.title}</h2>
+        <p>{details.description}</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  const handleMarkdownChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [currentDetails, setCurrentDetails] = useState({ title: '', description: '' });
+
+  const openPopup = (title, description) => {
+    setCurrentDetails({ title, description });
+    setPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setPopupOpen(false);
+  };
+
+  return (
+    <div className="App">
+      <div className='navbar h-12 flex justify-between'>
+        <div className="font-bold  text-400 text-[#26135c] text-4xl">
+          Markdown Previewer
+        </div>
+        <div className=' flex justify-evenly'>
+          <button className='mx-4 bg-black rounded-lg px-2 outline-none text-white' onClick={() => openPopup('Heading', <div><div className='text-[24px]'>'# h1'</div>
+          <div className='text-[20px]'>'## h2'</div><div className='text-[16px]'>'### h3'</div></div>)}>Heading</button>
+          
+          <button className='mx-4 bg-black rounded-lg px-2 outline-none text-white' onClick={() => openPopup('Link Syntax', <div className='text-bold text-red-500'>[Name](address)</div>)}>Link</button>
+          
+          <button className='mx-4 bg-black rounded-lg px-2 outline-none text-white' onClick={() => openPopup('Details Title', 'Some details related to this button.')}>Image</button>
+          
+          <button className='mx-4 bg-black rounded-lg px-2 outline-none text-white' onClick={() => openPopup('Details Title', 'Some details related to this button.')}>List</button>
+          
+          
+          {popupOpen && <Popup details={currentDetails} onClose={closePopup} />}
+   
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="row mt-4">
+          <div className="col-md-6 w-[800px]">
+            <h2>Input Markdown</h2>
+            <div className="mark-input w-[700px] h-[50vh]">
+              <textarea
+                className="input w-[700px] h-[50vh] border-[2px] border-solid
+                  border-gray-500 outline-none rounded-md bg-[#e8e3e387] ml-auto mr-auto p-2"
+                value={markdown}
+                onChange={handleMarkdownChange}
+              ></textarea>
+            </div>
+          </div>
+          <div className="col-md-6 w-[800px] mt-8">
+            <h2>Output</h2>
+            <div
+              className="mark-output block w-[700px] p-2 min-h-[500px] border-solid border-blue-500 rounded-md border-2px"
+              dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    './src/**/*.{html,js}',
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
